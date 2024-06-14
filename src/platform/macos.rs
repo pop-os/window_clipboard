03e0858e11d2a645bc97de803ad505a8d99fed1c
Bjorn Ove Hay Andersen <bjorn@crosspointlabs.com>
2024-06-14 11:40:21 +0200

    Added empty DndProvider for macos

--- src/platform/macos.rs
+++ src/platform/macos.rs
@@ -1,8 +1,10 @@
-use crate::ClipboardProvider;
+use crate::{dnd::DndProvider, ClipboardProvider};
 
 pub(crate) use clipboard_macos::Clipboard;
+use dnd::{DndAction, DndDestinationRectangle, DndSurface, Icon};
+use mime::{AllowedMimeTypes, AsMimeTypes};
 use raw_window_handle::HasDisplayHandle;
-use std::error::Error;
+use std::{borrow::Cow, error::Error};
 
 pub fn connect<W: HasDisplayHandle>(
     _window: &W,
@@ -17,5 +19,45 @@ impl ClipboardProvider for Clipboard {
 
     fn write(&mut self, contents: String) -> Result<(), Box<dyn Error>> {
         self.write(contents)
+    }
+}
+
+// TODO: Implement drag and drop for macos
+impl DndProvider for Clipboard {
+    fn init_dnd(
+        &self,
+        _tx: Box<dyn dnd::Sender<DndSurface> + Send + Sync + 'static>,
+    ) {
+    }
+
+    fn start_dnd<D: AsMimeTypes + Send + 'static>(
+        &self,
+        _internal: bool,
+        _source_surface: DndSurface,
+        _icon_surface: Option<Icon>,
+        _content: D,
+        _actions: DndAction,
+    ) {
+    }
+
+    fn end_dnd(&self) {}
+
+    fn register_dnd_destination(
+        &self,
+        _surface: DndSurface,
+        _rectangles: Vec<DndDestinationRectangle>,
+    ) {
+    }
+
+    fn set_action(&self, _action: DndAction) {}
+
+    fn peek_offer<D: AllowedMimeTypes + 'static>(
+        &self,
+        _mime_type: Option<Cow<'static, str>>,
+    ) -> std::io::Result<D> {
+        Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "DnD not supported",
+        ))
     }
 }
