f68595ee0e62fbd6589f4709b5aaa5c3c7ea5f6c
Ashley Wulber <ashley@system76.com>
2026-03-16 16:51:48 -0400

    cleanup: dbg

--- src/platform/linux.rs
+++ src/platform/linux.rs
@@ -74,7 +74,6 @@ impl ClipboardProvider for Clipboard {
     {
         match self {
             Clipboard::Wayland(c) => {
-                dbg!("linux write data...");
                 Some(c.write_data::<ClipboardStoreData<T>>(contents))
             }
             Clipboard::X11(_) => None,
